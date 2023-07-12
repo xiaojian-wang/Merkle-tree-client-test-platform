2cd8e15f00cb2725e9883f41f9cf22fd29a9248f
Xiaojian Wang <461005496@qq.com>
2023-07-12 11:48:40 -0400

    Add files via upload

---
+++ Merkle-tree-client-test-platform/Merkle_test_epoch_len_10_windows.py
@@ -0,0 +1,204 @@
+from concurrent import futures
+import sys
+import glob
+
+
+import os
+from merkletools import MerkleTools
+import hashlib
+import json
+import random
+import os
+import time
+
+intermediate_results = {}
+image_epoch_num = 0
+image_epoch = '0'
+proof_dict = {}
+# user id is just a random id is fine in test
+def generate_merkle_tree(user_id,result_file_path):  
+    result_file_path = glob.glob(result_file_path)[0] #       
+    with open(result_file_path, 'r') as f:
+        content = f.read()
+
+    clusters = content.split('\n\n')
+    # print("clusters: ", clusters)
+
+    for cluster in clusters:
+        results = []
+        if cluster != '':
+            results.append(cluster.strip())
+            # print("results: ", results)
+            # change the results list to string
+            results = results[0]
+            # print("results of one cluster: ", results)
+
+        else:
+            continue
+        # global intermediate_results
+        if user_id not in intermediate_results.keys():
+            intermediate_results[user_id] = {}
+        
+        if image_epoch_num not in intermediate_results[user_id].keys():
+            intermediate_results[user_id][image_epoch_num] = []
+            intermediate_results[user_id][image_epoch_num].append(results)
+            # print("aaa intermediate_results[user_id][image_epoch_num]: ", intermediate_results[user_id][image_epoch_num])
+        else:
+            intermediate_results[user_id][image_epoch_num].append(results)
+            # print("bbb intermediate_results[user_id][image_epoch_num]: ", intermediate_results[user_id][image_epoch_num])
+
+    # print("intermediate_results init: ", intermediate_results)
+
+
+            
+
+    def process_object_file(file_path):
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+
+        # Split the content by lines
+        lines = file_content.split('\n')
+
+        # Initialize result variables
+        num_objects = 0
+        # object_categories = {}
+        # object_motions = {}
+
+        # Process each line
+        for line in lines:
+            # Skip empty lines
+            if not line.strip():
+                continue
+
+            # Split line by spaces
+            values = line.strip().split()
+            # print("values[1]: ", values[1])
+
+            # Get the object index, category, and motion
+            object_index = int(float(values[1]))
+            num_objects = max(num_objects, object_index)
+
+        # Generate the final result string
+        result = f"Number of objects: {num_objects}\n\n"
+        return result
+
+    final_results = process_object_file(result_file_path)
+
+
+       
+    mt = MerkleTools() # mt = MerkleTools(hash_type="md5")  # default is sha256 
+    # global intermediate_results
+    intermediate_results[user_id][image_epoch_num].append(final_results)
+    # print("intermediate_results before add leaf: ", intermediate_results)
+    intermediate_results_tmp = intermediate_results[user_id][image_epoch_num]
+    mt.add_leaf(intermediate_results_tmp, True) # add all the intermediate results as the leaf nodes, True means the leaf nodes will be hashed
+    mt.make_tree() # construct the merkle tree
+    # print("root:", mt.get_merkle_root()) 
+
+    merkle_tree_root = mt.get_merkle_root() # get the root node of the merkle tree
+    return merkle_tree_root,mt
+
+def generate_proof(user_id,sample_rate,image_index_list,mt):
+    proof_result = {} # key: index, value: proof
+    image_results = {} # key: index, value: image result
+    for index in image_index_list:
+        proof = mt.get_proof(int(index))
+        proof_result[index] = proof
+        # print("type of image_epoch_num: ", type(image_epoch_num))
+        # print("type of index: ", type(index))
+
+        # global intermediate_results
+        # print("type of intermediate_results[user_id]: ", type(intermediate_results[user_id]))
+        # print("type of intermediate_results: ", type(intermediate_results))
+        # print("intermediate_results: ", intermediate_results)
+        # print("index: ", index)
+        image_results[index] = intermediate_results[user_id][image_epoch_num][int(index)]
+
+    proof_result_str = json.dumps(proof_result)
+    image_result_str = json.dumps(image_results)
+    
+    return proof_result_str,image_result_str
+
+def verify_proof(proof_str,image_result_str,merkle_tree_root):
+    proof = json.loads(proof_str)
+    proof_dict[image_epoch] = proof
+    image_results = json.loads(image_result_str)
+    # verify the proof
+    mt = MerkleTools()
+    
+    def validate_all_proofs(proof, image_results, merkle_tree_root):
+        for key, proof_value in proof.items():
+            leaf_node_hex = hashlib.sha256(str(image_results[key]).encode('utf-8')).hexdigest()
+            root = merkle_tree_root
+            if not mt.validate_proof(proof_value, leaf_node_hex, root):
+                return False
+        return True
+    
+    if validate_all_proofs(proof, image_results, merkle_tree_root):
+        # print("all the proof is valid!")
+        return True
+    else:
+        print("some proof is invalid!")
+        #break the program
+        # exit()
+        return False
+
+
+
+
+
+# if __name__ == '__main__':
+#     result_file_path = os.path.join('./results', image_epoch, '*.txt')
+#     sample_rate = 0.1
+#     image_index_list = [0,1,2,3,4,5,6,7,8,9] # based on sample rate to generate the image index list
+#     user_id = 'localhost:53097'
+#     merkle_tree_root,mt = generate_merkle_tree(user_id,result_file_path)
+#     proof_result_str,image_result_str = generate_proof(user_id,sample_rate,image_index_list,mt)
+#     verify_result = verify_proof(proof_result_str,image_result_str,merkle_tree_root)
+#     print("verify_result: ", verify_result)
+
+
+# def generate_image_index_list(sample_rate):
+#     image_index_list = list(range(10))
+#     num_samples = int(sample_rate * 10)
+#     image_index_list = image_index_list[:num_samples]
+#     return image_index_list
+
+def generate_image_index_list(sample_rate):
+    image_index_list = list(range(10))
+    num_samples = int(sample_rate * 10)
+    image_index_list = random.sample(image_index_list, num_samples)
+    return image_index_list
+
+def calculate_verify_time(sample_rate):
+    result_file_path = os.path.join('./results', image_epoch, '*.txt')
+    image_index_list = generate_image_index_list(sample_rate)
+    user_id = 'localhost:53097'
+    merkle_tree_root, mt = generate_merkle_tree(user_id, result_file_path)
+    proof_result_str, image_result_str = generate_proof(user_id, sample_rate, image_index_list, mt)
+
+    start_time = time.perf_counter()
+    verify_result = verify_proof(proof_result_str, image_result_str, merkle_tree_root)
+    end_time = time.perf_counter()
+    print("verify_result: ", verify_result)
+
+    verify_time = end_time - start_time
+    return verify_time
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sample_rate', type=float, required=True, help='Sample rate')
+    args = parser.parse_args()
+
+    sample_rate = args.sample_rate
+    image_index_list = generate_image_index_list(sample_rate)
+    print("image_index_list: ", image_index_list)
+    verify_time = calculate_verify_time(sample_rate)
+
+    result_filename = f"verify_time_{sample_rate}.txt"
+    with open(result_filename, 'a') as file:
+        # file.write(f"Sample Rate: {sample_rate}, Verify Time: {verify_time}\n")
+        file.write(f"{verify_time}\n")
+
+    # print("verify_result: ", verify_result)
--- Merkle-tree-client-test-platform/run_windows.sh
+++ Merkle-tree-client-test-platform/run_windows.sh
@@ -5,50 +5,50 @@
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 0.1
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 0.1
 done
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 0.2
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 0.2
 done
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 0.3
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 0.3
 done
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 0.4
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 0.4
 done
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 0.5
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 0.5
 done
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 0.6
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 0.6
 done
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 0.7
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 0.7
 done
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 0.8
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 0.8
 done
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 0.9
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 0.9
 done
 
 for ((j=1; j<=1000; j++))
 do
-	python Merkle_test_epoch_len_10.py --sample_rate 10
+	python Merkle_test_epoch_len_10_windows.py --sample_rate 10
 done
