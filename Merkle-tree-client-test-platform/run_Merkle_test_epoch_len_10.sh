16629a945614e604bd5c063dafaa87658b746de9
Xiaojian Wang <461005496@qq.com>
2023-07-12 11:13:47 -0400

    Update run_Merkle_test_epoch_len_10.sh

--- Merkle-tree-client-test-platform/run_Merkle_test_epoch_len_10.sh
+++ Merkle-tree-client-test-platform/run_Merkle_test_epoch_len_10.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Delete existing files if they exist
-for i in {1..10}
-do
-    sample_rate=$(bc <<< "scale=1; $i/10")
-    filename=$(printf "verify_time_%.1f.txt" $sample_rate)
-    if [[ -f $filename ]]; then
-        rm $filename
-    fi
-done
+# for i in {1..10}
+# do
+#     sample_rate=$(bc <<< "scale=1; $i/10")
+#     filename=$(printf "verify_time_%.1f.txt" $sample_rate)
+#     if [[ -f $filename ]]; then
+#         rm $filename
+#     fi
+# done
 
 # Run the script 1000 times with increasing sample_rate of 0.1 each time
 for i in {1..10}
