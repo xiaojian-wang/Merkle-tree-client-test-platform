0475bc58b88edec1412d02ff1ca2f1b5f8e358ba
Xiaojian Wang <461005496@qq.com>
2023-07-22 18:17:43 -0400

    Update Merkle_test_epoch_len_10.py

--- Merkle-tree-client-test-platform/Merkle_test_epoch_len_10.py
+++ Merkle-tree-client-test-platform/Merkle_test_epoch_len_10.py
@@ -110,7 +110,7 @@ def generate_merkle_tree(user_id,result_file_path):
     intermediate_results[user_id][image_epoch_num].append(final_results)
     # print("intermediate_results before add leaf: ", intermediate_results)
     intermediate_results_tmp = intermediate_results[user_id][image_epoch_num]
-    hash_of_input_image = hash_of_all_input_images[user_id][image_epoch]
+    # hash_of_input_image = hash_of_all_input_images[user_id][image_epoch]
     merkle_tree_leaf_string = [x + y for x, y in zip(intermediate_results_tmp, hash_of_input_image)]
 
     mt.add_leaf(merkle_tree_leaf_string, True) # add all the intermediate results as the leaf nodes, True means the leaf nodes will be hashed
